08f2950083641d8006f3ebada5b451887ca09163
mcpengelly <pengelly.mat@gmail.com>
2016-10-08 18:45:01 -0400

    being implementing check3x3grid, some refactoring

--- src/solver.js
+++ src/solver.js
@@ -48,29 +48,42 @@ module.exports = {
 
 	//checks all squares in the current row
 	checkRows: function(pBoard, row, value){
-		var match = false;
+		var canInsert = true;
 		for(var i = 0; i < pBoard.length; i++){
 			if(value === pBoard[row][i]){
-				match = true;
+				canInsert = false;
 			}
 		}
-		return match;
+		return canInsert;
 	},
 
 	//checks all squares in the current column
 	checkColumns: function(pBoard, column, value){
-		var match = false;
+		var canInsert = true;
 		for(var i = 0; i < pBoard[0].length; i++){
 			if(value === pBoard[i][column]){
-				match = true;
+				canInsert = false;
 			}
 		}
-		return match;
+		return canInsert;
 	},
 
 	//checks all squares in the current grid/region
 	check3x3Grid: function(pBoard, row, column, value){
 		//how to check if the squares are within a specific region?
+		//then checks if it can be input into that region by iterating through it
+		var region = 0;
+		if(row <= 2 && column <= 2) {
+			//belongs to the first 3x3 grid
+			region = 1;
+		}
+
+		if(region === 1){
+			for(var i = 0; i <= 2; i++){
+				// console.log(pBoard[row][i]);
+				console.log(pBoard[i][column]);
+			}
+		}
 	},
 
 	//solves the grid/puzzle portion of the sudoku
